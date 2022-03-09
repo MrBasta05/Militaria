f2a663304f8fc5b62728bc6be0b0b2806a198437
TutusBOT <barte77pl@gmail.com>
2022-03-09 21:23:25 +0100

    added currency after cost

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function displayMainProducts() {
 		console.log("test", mainProducts[i].children, res[i]);
 		mainProducts[i].children[0].innerText = res[i].nazwa;
 		mainProducts[i].children[1].innerText = res[i].opis;
-		mainProducts[i].children[2].innerText = res[i].koszt;
+		mainProducts[i].children[2].innerText = `${res[i].koszt}zł`;
 		mainProducts[i].children[3].src = `${res[i].id || "placeholder"}.jpg`;
 	}
 }
