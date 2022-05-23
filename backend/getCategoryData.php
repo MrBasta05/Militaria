a16ffcd154bfc468cf7ef9a45c3614248bb61c25
TutusBOT <barte77pl@gmail.com>
2022-05-23 20:53:51 +0200

    test

--- backend/getCategoryData.php
+++ backend/getCategoryData.php
@@ -1,3 +1,10 @@
 <?php
 $categoryId = $_GET['category'];
-echo $categoryId;
+$con = mysqli_connect('localhost', 'root', '', 'Militaria');
+$queryCategoryName = "SELECT nazwa FROM kategoria WHERE id = ".$categoryId;
+// $categoryName  = mysqli(mysqli_query($con, $queryCategoryName));
+
+$categoryName = $con->query($queryCategoryName)->fetch_object()->nazwa;
+
+mysqli_close($con);
+echo $categoryName;
