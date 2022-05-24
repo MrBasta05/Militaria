412189c68f92837a6179b72b1d7d8e43a0d9392a
TutusBOT <barte77pl@gmail.com>
2022-05-24 19:48:09 +0200

    listeners on all remove buttons

--- frontend/js/koszyk/index.js
+++ frontend/js/koszyk/index.js
@@ -8,13 +8,27 @@ getCartData(userId);
 // Removes all cart items if data is successfully removed from database
 const cartContainer = document.querySelector(".cart-container");
 const removeAllButton = document.getElementById("removeAllButton");
+const cartItemsElements = [];
+[...cartContainer.children].forEach((element) => {
+	if (element.className === "cart-items") {
+		cartItemsElements.push(element);
+	}
+});
+console.log(cartItemsElements);
 removeAllButton.addEventListener("click", async () => {
 	const isRemoved = await handleRemoveAll();
 	if (isRemoved) {
 		[...cartContainer.children].forEach((element) => {
-			if (element.className === "cart-items") {
+			if (cartItemsElements.includes(element)) {
 				cartContainer.removeChild(element);
 			}
 		});
 	}
+});
+
+cartItemsElements.forEach((element) => {
+	const removeItemButton = element.querySelector(".remove");
+	removeItemButton.addEventListener("click", () => {
+		console.log(element);
+	});
 });
