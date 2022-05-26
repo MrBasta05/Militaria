cac00a4d58a4c97bcb24ebff3e9bc3e3a6a497f2
Marcel Mika <marcel.mika2003@gmail.com>
2022-05-26 18:06:32 +0200

    SUPERQUERY

--- backend/getCartData.php
+++ backend/getCartData.php
@@ -2,5 +2,5 @@
 $con = mysqli_connect('localhost', 'root', '', 'Militaria');
 
 // DZORDAN query ktore pobiera uzytkownika a nastepnie pobiera zawartość jego koszyka
-
+//SELECT produkt.nazwa, produkt.koszt, zawartosc_koszyka.ilosc from uzytkownik join zawartosc_koszyka join produkt on uzytkownik.id = zawartosc_koszyka.uzytkownik_id and zawartosc_koszyka.produkt_id = produkt.id where uzytkownik.nazwa = "DESTRUKTOID69";
 mysqli_close($con);
