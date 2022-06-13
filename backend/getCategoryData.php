78fc8f3615d8893d8018c1b8d9a1433b296469d1
TutusBOT <barte77pl@gmail.com>
2022-06-13 17:47:56 +0200

    erfon nie umie dodac

--- backend/getCategoryData.php
+++ backend/getCategoryData.php
@@ -1,9 +1,14 @@
 <?php
 $categoryId = $_GET['category'];
-$con = mysqli_connect('localhost', 'root', '', 'Militaria');
+$con = mysqli_connect('localhost', 'root', '', 'militaria');
 $queryCategoryName = "SELECT nazwa FROM kategoria WHERE id = " . $categoryId;
-$queryProducts = ""; // DZORDAN query które pobiera ileś tam produktów z kategorii o id $categoryId
+$queryProducts = "SELECT produkt.nazwa from produkt join podkategoria join kategoria on produkt.podkategoria_id = podkategoria.id and podkategoria.kategoria_id = kategoria.id where kategoria.id = " . $categoryId . ";"; // DZORDAN query które pobiera ileś tam produktów z kategorii o id $categoryId
 $categoryName = $con->query($queryCategoryName)->fetch_object()->nazwa;
-
+$reqProd = mysqli_query($con, $queryProducts);
+$wynik = array();
+$wynik[] = $categoryName;
+while ($data = mysqli_fetch_assoc($reqProd)) {
+    $wynik[] = $data;
+};
+echo json_encode($wynik);
 mysqli_close($con);
-echo $categoryName;
--- frontend/js/koszyk/index.js
+++ frontend/js/koszyk/index.js
@@ -42,6 +42,70 @@ async function cartData() {
 		cartItemsElements.push(cartItems);
 		document.querySelector(".checkout").before(cartItems);
 	});
+
+	cartItemsElements.forEach((element) => {
+		const itemName = element.querySelector(".title").innerText;
+		// guzik usuń w koszyku
+		const removeItemButton = element.querySelector(".remove");
+		removeItemButton.addEventListener("click", async () => {
+			const isRemoved = await handleRemoveOne(itemName);
+			if (isRemoved) {
+				cartContainer.removeChild(element);
+			} else {
+				console.error("Nie udało się usunąć produktu.");
+			}
+		});
+
+		// guziki + i - w koszyku
+		const incrItemButton = element.querySelector(".btn-incr");
+		incrItemButton.addEventListener("click", async () => {
+			let amount = parseInt(element.querySelector(".count").innerText);
+			const isChanged = await handleChangeAmount(itemName);
+			if (isChanged) {
+				element.querySelector(".count").innerText = amount + 1;
+			} else {
+				console.error("Błąd");
+			}
+		});
+		const decrItemButton = element.querySelector(".btn-decr");
+		decrItemButton.addEventListener("click", async () => {
+			let amount = parseInt(element.querySelector(".count").innerText);
+			if (amount === 1) {
+				const isRemoved = await handleRemoveOne(itemName);
+				if (isRemoved) {
+					cartContainer.removeChild(element);
+				} else {
+					console.error("Nie udało się usunąć produktu.");
+				}
+			} else {
+				const isChanged = await handleChangeAmount(itemName);
+				if (isChanged) {
+					element.querySelector(".count").innerText = amount - 1;
+				} else {
+					console.error("Błąd");
+				}
+			}
+		});
+
+		const payButton = document.getElementById("btn-pay");
+		payButton.addEventListener("click", async () => {
+			console.log("handle pay");
+			const isPaid = await handlePay();
+			if (isPaid) {
+				const isRemoved = handleRemoveAll();
+				if (isRemoved) {
+					removeAllCartElements(
+						[...cartContainer.children],
+						cartItemsElements,
+						cartContainer
+					);
+					console.log("payment successfull");
+				}
+			} else {
+				console.error("Błąd z płatnością");
+			}
+		});
+	});
 }
 cartData();
 // Removes all cart items if data is successfully removed from database
@@ -58,68 +122,4 @@ removeAllButton.addEventListener("click", async () => {
 			cartContainer
 		);
 	}
-});
-
-cartItemsElements.forEach((element) => {
-	const itemName = element.querySelector(".title").innerText;
-	// guzik usuń w koszyku
-	const removeItemButton = element.querySelector(".remove");
-	removeItemButton.addEventListener("click", async () => {
-		const isRemoved = await handleRemoveOne(itemName);
-		if (isRemoved) {
-			cartContainer.removeChild(element);
-		} else {
-			console.error("Nie udało się usunąć produktu.");
-		}
-	});
-
-	// guziki + i - w koszyku
-	const incrItemButton = element.querySelector(".btn-incr");
-	incrItemButton.addEventListener("click", async () => {
-		let amount = parseInt(element.querySelector(".count").innerText);
-		const isChanged = await handleChangeAmount(itemName);
-		if (isChanged) {
-			element.querySelector(".count").innerText = amount + 1;
-		} else {
-			console.error("Błąd");
-		}
-	});
-	const decrItemButton = element.querySelector(".btn-decr");
-	decrItemButton.addEventListener("click", async () => {
-		let amount = parseInt(element.querySelector(".count").innerText);
-		if (amount === 1) {
-			const isRemoved = await handleRemoveOne(itemName);
-			if (isRemoved) {
-				cartContainer.removeChild(element);
-			} else {
-				console.error("Nie udało się usunąć produktu.");
-			}
-		} else {
-			const isChanged = await handleChangeAmount(itemName);
-			if (isChanged) {
-				element.querySelector(".count").innerText = amount - 1;
-			} else {
-				console.error("Błąd");
-			}
-		}
-	});
-
-	const payButton = document.getElementById("btn-pay");
-	payButton.addEventListener("click", async () => {
-		console.log("handle pay");
-		const isPaid = await handlePay();
-		if (isPaid) {
-			const isRemoved = handleRemoveAll();
-			if (isRemoved) {
-				removeAllCartElements(
-					[...cartContainer.children],
-					cartItemsElements,
-					cartContainer
-				);
-				console.log("payment successfull");
-			}
-		} else {
-			console.error("Błąd z płatnością");
-		}
-	});
 });
